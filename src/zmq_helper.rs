089055f5fce35c095573ed8d6be3846ef72cc899
mgaertne <mgaertne@gmail.com>
2025-06-26 10:10:26 +0200

    Revert "probably fixed the last build"
    This reverts commit 744f8d9750e3b558a655887e14e7d938acc01cba.

--- src/zmq_helper.rs
+++ src/zmq_helper.rs
@@ -1,28 +1,27 @@
-use core::sync::atomic::Ordering;
+use core::sync::atomic::{AtomicBool, Ordering};
 
 use anyhow::Result;
 use rzmq::{
-    Context, Msg, Socket, SocketType, ZmqError,
+    Context, Socket, SocketType,
     socket::{PLAIN_PASSWORD, PLAIN_USERNAME, RCVTIMEO, ROUTING_ID, SocketEvent, ZAP_DOMAIN},
 };
-use rzmq::socket::SNDTIMEO;
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender, error::TryRecvError};
 use uuid::Uuid;
 
 use crate::{CONTINUE_RUNNING, cmd_line::CommandLineOptions};
 
-pub(crate) async fn create_socket(
+pub(crate) fn create_socket(
     context: &Context,
     password: &str,
     identity: &str,
 ) -> Result<Socket, rzmq::ZmqError> {
     let socket = context.socket(SocketType::Dealer)?;
 
-    socket.set_option(PLAIN_USERNAME, "rcon").await?;
+    socket.set_option(PLAIN_USERNAME, Some("rcon"))?;
     if !password.is_empty() {
-        socket.set_option(PLAIN_PASSWORD, password).await?;
+        socket.set_option(PLAIN_PASSWORD, Some(password))?;
     } else {
-        socket.set_option(PLAIN_PASSWORD, "").await?;
+        socket.set_option(PLAIN_PASSWORD, None)?;
     }
 
     let identity_str = if identity.is_empty() {
@@ -32,14 +31,11 @@ pub(crate) async fn create_socket(
         identity.to_string()
     };
 
-    socket
-        .set_option(ROUTING_ID, identity_str.as_bytes())
-        .await?;
+    socket.set_option(ROUTING_ID, identity_str.as_bytes())?;
 
-    socket.set_option(ZAP_DOMAIN, "rcon").await?;
+    socket.set_option(ZAP_DOMAIN, "rcon")?;
 
-    socket.set_option(RCVTIMEO, 100).await?;
-    socket.set_option(SNDTIMEO, 100).await?;
+    socket.set_option(RCVTIMEO, 100)?;
 
     Ok(socket)
 }
@@ -58,80 +54,88 @@ pub(crate) async fn run_zmq(
     display_sender.send(format!("ZMQ connecting to {}...", &args.host))?;
 
     let zmq_context = Context::new()?;
-    let socket = create_socket(&zmq_context, &args.password, &args.identity).await?;
+    let socket = create_socket(&zmq_context, &args.password, &args.identity)?;
 
-    let monitor_socket = socket.monitor_default().await?;
+    let monitor_socket = socket.monitor_default()?;
 
-    socket.connect(&args.host).await?;
+    socket.connect(&args.host)?;
 
-    'outer: loop {
+    let first = AtomicBool::new(true);
+
+    while let Ok(event) = socket.recv().await {
         if !CONTINUE_RUNNING.load(Ordering::SeqCst) {
-            break 'outer;
+            break;
         }
 
-        match socket.recv().await {
-            Ok(event) => {
-                match monitor_socket.recv().await {
-                    Ok(SocketEvent::Connected { .. }) => {
-                        display_sender.send("ZMQ registering with the server.".to_string())?;
-                        if let Err(e) = socket.send(Msg::from_static("register".as_bytes())).await {
+        match monitor_socket.recv_multipart() {
+            Ok(zmq_msg) => {
+                let event_id = zmq_msg.get_event_id();
+
+                match event_id {
+                    SocketEvent::ConnectDelayed { .. } => {
+                        if first.load(Ordering::SeqCst) {
+                            first.store(false, Ordering::SeqCst);
+                            display_sender.send("ZMQ registering with the server.".to_string())?;
+                        }
+                        if let Err(e) = socket.send("register").await {
                             display_sender.send(format!("error registering with ZMQ: {e:?}."))?;
                         }
                     }
-                    Ok(SocketEvent::ConnectDelayed { .. })
-                    | Ok(SocketEvent::ConnectRetried { .. }) => {
-                        continue 'outer;
+                    SocketEvent::ConnectDelayed { .. } | SocketEvent::ConnectRetried { .. } => {
+                        continue;
                     }
-                    Ok(SocketEvent::HandshakeSucceeded { .. }) => {
+                    SocketEvent::HandshakeSucceeded { .. } => {
+                        first.store(true, Ordering::SeqCst);
                         display_sender.send(format!("ZMQ connected to {}.", &args.host))?;
                     }
-                    Ok(SocketEvent::HandshakeFailed { .. }) | Ok(SocketEvent::Closed { .. }) => {
-                        break 'outer;
-                    }
-                    Ok(SocketEvent::Disconnected { endpoint }) => {
-                        if let Err(e) = socket.connect(&endpoint).await {
+                    SocketEvent::HandshakeFailed { .. } | SocketEvent::Closed { .. } => break,
+                    SocketEvent::Disconnected { .. } => {
+                        if first.load(Ordering::SeqCst) {
+                            first.store(false, Ordering::SeqCst);
+                            display_sender.send("Reconnecting ZMQ...".to_string())?;
+                        }
+                        if let Err(e) = socket.connect(&args.host) {
                             display_sender.send(format!("error reconnecting: {e:?}."))?;
                         }
                     }
-                    Ok(socket_event) => {
-                        display_sender.send(format!("{socket_event:?}",))?;
-                    }
-                    Err(e) => {
-                        display_sender.send(format!("zmq error: {e:?}"))?;
-                    }
-                }
-
-                'inner: loop {
-                    match zmq_receiver.try_recv() {
-                        Ok(line) => {
-                            socket.send(Msg::from_vec(line.into_bytes())).await?;
-                        }
-                        Err(TryRecvError::Disconnected) => {
-                            display_sender.send("receiver disconnected".to_string())?;
-                            CONTINUE_RUNNING.store(false, Ordering::SeqCst);
-                            break 'inner;
-                        }
-                        _ => {
-                            break 'inner;
-                        }
-                    }
-                }
-
-                if event.data().is_none() {
-                    continue 'outer;
-                }
-
-                while let Ok(zmq_msg) = socket.recv().await {
-                    if let Some(zmq_data) = zmq_msg.data() {
-                        let zmq_str = String::from_utf8_lossy(zmq_data);
-                        display_sender.send(trim_ql_msg(&zmq_str))?;
+                    socket_event => {
+                        display_sender.send(format!(
+                            "{:#06x?} {:?}",
+                            socket_event.to_raw(),
+                            socket_event
+                        ))?;
                     }
                 }
             }
-            Err(ZmqError::Timeout) => {
-            },
             Err(e) => {
-                display_sender.send(format!("ZmqError: {e:?}"))?;
+                display_sender.send(format!("zmq error: {e:?}"))?;
+            }
+        }
+
+        loop {
+            match zmq_receiver.try_recv() {
+                Ok(line) => {
+                    socket.send(&line).await?;
+                }
+                Err(TryRecvError::Disconnected) => {
+                    display_sender.send("receiver disconnected".to_string())?;
+                    CONTINUE_RUNNING.store(false, Ordering::SeqCst);
+                    break;
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+
+        if event == 0 {
+            continue;
+        }
+
+        while let Ok(zmq_msg) = socket.recv().await {
+            if let Some(zmq_data) = zmq_msg.data() {
+                let zmq_str = String::from(zmq_data);
+                display_sender.send(trim_ql_msg(zmq_str))?;
             }
         }
     }
@@ -142,8 +146,8 @@ pub(crate) async fn run_zmq(
         display_sender.send("Exiting ZMQ...".to_string())?;
     }
 
-    monitor_socket.close()?;
-    socket.close().await?;
+    monitor_socket.disconnect()?;
+    socket.disconnect(&args.host)?;
 
     drop(display_sender);
 
