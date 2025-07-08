40d881a3530681c86f3870b3a35fec752dbc5e33
mgaertne <mgaertne@gmail.com>
2025-07-08 19:44:18 +0200

    added request-reply example for async-std

---
+++ azmq/examples/rustdoc/futures/async-std/request_reply.rs
@@ -0,0 +1,69 @@
+#![cfg(feature = "examples-async-std")]
+use core::sync::atomic::{AtomicBool, AtomicI32, Ordering};
+
+use async_std::task;
+use azmq::{
+    ZmqResult,
+    context::ZmqContext,
+    futures::{AsyncZmqReceiver, AsyncZmqSender},
+    socket::{Reply, Request, ZmqSendFlags, ZmqSocket},
+};
+use futures::join;
+
+static KEEP_RUNNING: AtomicBool = AtomicBool::new(true);
+static ITERATIONS: AtomicI32 = AtomicI32::new(0);
+
+async fn run_replier(reply: ZmqSocket<Reply>) -> ZmqResult<()> {
+    while KEEP_RUNNING.load(Ordering::Acquire) {
+        if let Some(message) = reply.recv_msg_async().await {
+            println!("Received request: {message}");
+            reply.send_msg_async("World", ZmqSendFlags::empty()).await;
+        }
+    }
+
+    Ok(())
+}
+
+async fn run_requester(request: ZmqSocket<Request>) -> ZmqResult<()> {
+    while ITERATIONS.load(Ordering::Acquire) > 0 {
+        let request_no = ITERATIONS.load(Ordering::Acquire);
+        println!("Sending request {request_no}");
+        let _ = request.send_msg_async("Hello", ZmqSendFlags::empty()).await;
+
+        loop {
+            if let Some(message) = request.recv_msg_async().await {
+                println!("Received reply {request_no:2} [{message}]");
+
+                ITERATIONS.fetch_sub(1, Ordering::Release);
+
+                break;
+            }
+        }
+    }
+
+    KEEP_RUNNING.store(false, Ordering::Release);
+
+    Ok(())
+}
+
+#[async_std::main]
+async fn main() -> ZmqResult<()> {
+    ITERATIONS.store(10, Ordering::Release);
+
+    let port = 5556;
+
+    let context = ZmqContext::new()?;
+
+    let reply = ZmqSocket::<Reply>::from_context(&context)?;
+    reply.bind(format!("tcp://*:{port}"))?;
+
+    let request = ZmqSocket::<Request>::from_context(&context)?;
+    request.connect(format!("tcp://localhost:{port}"))?;
+
+    let request_handle = task::spawn(run_requester(request));
+    let reply_handle = task::spawn(run_replier(reply));
+
+    let _ = join!(reply_handle, request_handle);
+
+    Ok(())
+}
