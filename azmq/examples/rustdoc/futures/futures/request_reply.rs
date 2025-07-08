73f6a1f14c448bcf0b72a42d0ba415150049e35e
mgaertne <mgaertne@gmail.com>
2025-07-08 19:31:42 +0200

    added a request reply example for tokio

---
+++ azmq/examples/rustdoc/futures/futures/request_reply.rs
@@ -0,0 +1,71 @@
+#![cfg(feature = "examples-tokio")]
+
+use core::sync::atomic::{AtomicBool, AtomicI32};
+use core::sync::atomic::Ordering;
+
+use azmq::{
+    ZmqResult,
+    context::ZmqContext,
+    futures::{AsyncZmqReceiver, AsyncZmqSender},
+    socket::{ZmqSendFlags, ZmqSocket},
+};
+use tokio::{join, task};
+use azmq::socket::{Reply, Request};
+
+static KEEP_RUNNING: AtomicBool = AtomicBool::new(true);
+static ITERATIONS: AtomicI32 = AtomicI32::new(0);
+
+async fn run_replier(reply: ZmqSocket<Reply>) -> ZmqResult<()> {
+    while KEEP_RUNNING.load(Ordering::Acquire) {
+        if let Some(message) = reply.recv_msg_async().await {
+            println!("Received request: {message}");
+            reply.send_msg_async("World", ZmqSendFlags::empty()).await;
+        }
+    }
+
+    Ok(())
+}
+
+async fn run_requester(request: ZmqSocket<Request>) -> ZmqResult<()> {
+    while ITERATIONS.load(Ordering::Acquire) > 0 {
+        let request_no = ITERATIONS.load(Ordering::Acquire);
+        println!("Sending request {request_no}");
+        let _ = request.send_msg_async("Hello", ZmqSendFlags::empty()).await;
+
+        loop {
+            if let Some(message) = request.recv_msg_async().await {
+                println!("Received reply {request_no:2} [{message}]");
+
+                ITERATIONS.fetch_sub(1, Ordering::Release);
+
+                break;
+            }
+        }
+    }
+
+    KEEP_RUNNING.store(false, Ordering::Release);
+
+    Ok(())
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 2)]
+async fn main() -> ZmqResult<()> {
+    ITERATIONS.store(10, Ordering::Release);
+
+    let port = 5556;
+
+    let context = ZmqContext::new()?;
+
+    let reply = ZmqSocket::<Reply>::from_context(&context)?;
+    reply.bind(format!("tcp://*:{port}"))?;
+
+    let request = ZmqSocket::<Request>::from_context(&context)?;
+    request.connect(format!("tcp://localhost:{port}"))?;
+
+    let request_handle = task::spawn(run_requester(request));
+    let reply_handle = task::spawn(run_replier(reply));
+
+    let _ = join!(reply_handle, request_handle);
+
+    Ok(())
+}
