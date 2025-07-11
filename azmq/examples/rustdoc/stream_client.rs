756d414e8620ce45aa47513249db1cdcec9bd488
Markus Gärtner <markus.gaertner@it-agile.de>
2025-07-11 14:18:57 +0200

    added a first stream_client example after adding the documentation parts for the stream socket

---
+++ azmq/examples/rustdoc/stream_client.rs
@@ -0,0 +1,57 @@
+use std::{io::prelude::*, net::TcpListener, thread};
+
+use azmq::{
+    ZmqResult,
+    context::Context,
+    message::MultipartMessage,
+    socket::{Receiver, RecvFlags, SendFlags, Sender, Socket, Stream},
+};
+
+fn main() -> ZmqResult<()> {
+    let port = 5556;
+
+    let tcp_listener = TcpListener::bind(format!("127.0.0.1:{port}")).unwrap();
+    thread::spawn(move || {
+        let (mut tcp_stream, _socket_addr) = tcp_listener.accept().unwrap();
+        tcp_stream.write("".as_bytes()).unwrap();
+        loop {
+            let mut buffer = [0; 256];
+            if let Ok(length) = tcp_stream.read(&mut buffer) {
+                if length == 0 {
+                    break;
+                }
+                let recevied_msg = &buffer[..length];
+                println!(
+                    "Received request: {}",
+                    str::from_utf8(recevied_msg).unwrap()
+                );
+                tcp_stream.write("World".as_bytes()).unwrap();
+            }
+        }
+    });
+
+    let context = Context::new()?;
+
+    let zmq_stream = Socket::<Stream>::from_context(&context)?;
+    zmq_stream.connect(format!("tcp://127.0.0.1:{port}"))?;
+    let mut connect_msg = zmq_stream.recv_multipart(RecvFlags::empty())?;
+    let routing_id = connect_msg.pop_front().unwrap();
+
+    for request_no in 1..=10 {
+        let mut multipart = MultipartMessage::new();
+        multipart.push_back(routing_id.clone());
+        multipart.push_back("Hello".into());
+        println!("Sending request {request_no}");
+        zmq_stream.send_multipart(multipart, SendFlags::empty())?;
+
+        let mut message = zmq_stream.recv_multipart(RecvFlags::empty())?;
+        println!(
+            "Received reply {request_no:2} {}",
+            message.pop_back().unwrap()
+        );
+    }
+
+    zmq_stream.disconnect(format!("tcp://localhost:{port}"))?;
+
+    Ok(())
+}
--- azmq/src/socket/stream.rs
+++ azmq/src/socket/stream.rs
@@ -17,6 +17,35 @@ impl sealed::SocketType for Stream {
 unsafe impl Sync for Socket<Stream> {}
 unsafe impl Send for Socket<Stream> {}
 
+/// # A stream socket `ZMQ_STREAM`
+///
+/// A socket of type [`Stream`] is used to send and receive TCP data from a non-0MQ peer, when
+/// using the tcp:// transport. A [`Stream`] socket can act as client and/or server, sending
+/// and/or receiving TCP data asynchronously.
+///
+/// When receiving TCP data, a [`Stream`] socket shall prepend a message part containing the
+/// routing id of the originating peer to the message before passing it to the application.
+/// Messages received are fair-queued from among all connected peers.
+///
+/// When sending TCP data, a [`Stream`] socket shall remove the first part of the message and use
+/// it to determine the routing id of the peer the message shall be routed to, and unroutable
+/// messages shall cause an
+/// `Err(`[`ZmqError::HostUnreachable`](crate::ZmqError::HostUnreachable)`)` or
+/// `Err(`[`ZmqError::Again`](crate::ZmqError::Again)`)` error.
+///
+/// To open a connection to a server, use the [`connect()`](method@Socket::connect()) call, and
+/// then fetch the socket routing id using [`routing_id()`](method@Socket::routing_id())
+/// option.
+///
+/// To close a specific connection, send the routing id frame followed by a zero-length message.
+///
+/// When a connection is made, a zero-length message will be received by the application.
+/// Similarly, when the peer disconnects (or the connection is lost), a zero-length message will
+/// be received by the application.
+///
+/// You must send one routing id frame followed by one data frame.
+/// The [`SendFlags::SEND_MORE`](const@super::SendFlags::SEND_MORE) flag is required for routing
+/// id frames but is ignored on data frames.
 impl Socket<Stream> {
     pub fn set_routing_id<T: AsRef<str>>(&self, value: T) -> ZmqResult<()> {
         self.set_sockopt_string(SocketOptions::RoutingId as i32, value)
