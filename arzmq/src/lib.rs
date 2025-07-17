d08fb527e72658255963f1d72530ab96abf1a174
Markus Gärtner <markus.gaertner@it-agile.de>
2025-07-18 00:47:10 +0200

    this clippy remark seems new to me, moved around the unittests to satisfy clippy

--- arzmq/src/lib.rs
+++ arzmq/src/lib.rs
@@ -94,29 +94,6 @@ pub fn has_capability(capability: Capability) -> bool {
     unsafe { zmq_sys_crate::zmq_has(c_str.as_ptr()) != 0 }
 }
 
-#[cfg(test)]
-mod has_capability_tests {
-    use super::{Capability, has_capability};
-
-    #[test]
-    fn has_ipc_capability() {
-        assert!(has_capability(Capability::Ipc));
-    }
-
-    #[test]
-    fn has_curve_capability() {
-        assert_eq!(has_capability(Capability::Curve), cfg!(feature = "curve"));
-    }
-
-    #[test]
-    fn has_draft_capability() {
-        assert_eq!(
-            has_capability(Capability::Draft),
-            cfg!(feature = "draft-api")
-        );
-    }
-}
-
 /// Return the current zeromq version, as `(major, minor, patch)`.
 pub fn version() -> (i32, i32, i32) {
     let mut major = Default::default();
@@ -183,4 +160,27 @@ pub fn proxy<T: sealed::SocketType>(
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod has_capability_tests {
+    use super::{Capability, has_capability};
+
+    #[test]
+    fn has_ipc_capability() {
+        assert!(has_capability(Capability::Ipc));
+    }
+
+    #[test]
+    fn has_curve_capability() {
+        assert_eq!(has_capability(Capability::Curve), cfg!(feature = "curve"));
+    }
+
+    #[test]
+    fn has_draft_capability() {
+        assert_eq!(
+            has_capability(Capability::Draft),
+            cfg!(feature = "draft-api")
+        );
+    }
 }
