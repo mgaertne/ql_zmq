2dfd5b406df416a89e179214f2c278d08bf3637d
mgaertne <mgaertne@gmail.com>
2025-07-20 18:06:21 +0200

    finally got to fix the windows build

--- arzmq-sys/build.rs
+++ arzmq-sys/build.rs
@@ -1,10 +1,13 @@
 use std::{env, path::PathBuf};
 
 fn main() {
+    #[cfg(windows)]
+    println!("cargo::rustc-link-lib=Advapi32");
+
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed=PROFILE");
 
-    let maybe_libsodium = if cfg!(feature = "libsodium") {
+    let maybe_libsodium = if cfg!(all(not(windows), feature = "libsodium")) {
         let lib_dir = env::var("DEP_SODIUM_LIB").expect("build metadata `DEP_SODIUM_LIB` required");
         let include_dir =
             env::var("DEP_SODIUM_INCLUDE").expect("build metadata `DEP_SODIUM_INCLUDE` required");
